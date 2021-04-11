ed9a6ebf9962ed0232056af4f1bf867e4e403425
Mark Gregory <mark.gregory@mccann.com>
2021-04-11 17:20:15 +0100

    Added Logbook Module

---
+++ Modules/User/Database/Migrations/2021_04_11_161213_UpdateUsersTable.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('callsign')->nullable();
+            $table->string('telephone')->nullable();
+            $table->string('mobile')->nullable();
+            $table->integer('location')->unsigned()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

7d55cb16b6840f0697c05e004682e2bdfd611060
Mark Gregory <mark.gregory@mccann.com>
2021-04-15 23:15:32 +0100

    Added logbook API support

---
+++ Modules/Logbook/Http/Controllers/Api/v1/LogbookController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Modules\Logbook\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Log;
+use Modules\Logbook\Entities\Logbook;
+
+class LogbookController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return |null
+     */
+    public function store(Request $request)
+    {
+        if (in_array($request->method(), ['POST'])
+            && $request->isJson()
+        ) {
+            $data = $request->json()->all();
+            $logEntry = Logbook::create($data);
+
+            $response = \Modules\Logbook\Http\Controllers\LogbookController::hamQTH($logEntry->call);
+
+            if ($response['dxcc']['adif'] != '0') {
+                $logEntry->addCallDetails($response);
+            } else {
+                $logEntry->save();
+            }
+
+            Log::info('New Log Entry Created for '.$logEntry->call);
+        }
+
+        return null;
+    }
+
+    public function storeMacLogger(Request $request)
+    {
+        log::info('Called Store MacLogger');
+
+        if (in_array($request->method(), ['POST'])
+            && $request->isJson()
+        ) {
+            $data = $request->json()->all();
+            $entry = json_decode($data, true);
+            $logEntry = new Logbook();
+            $logEntry->save($entry);
+
+            $response = \Modules\Logbook\Http\Controllers\LogbookController::hamQTH($logEntry->call);
+
+            if ($response['dxcc']['adif'] != '0') {
+                $logEntry->addCallDetails($response);
+            } else {
+                $logEntry->save();
+            }
+
+            Log::info('New Log Entry Created for '.$data['CALL']);
+        }
+    }
+}

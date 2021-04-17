2ad697a2a087d72718f3e7d6438241a6b7a7b495
Mark Gregory <mark.gregory@mccann.com>
2021-04-17 19:15:47 +0100

    Style Fixes

--- deploy.php
+++ deploy.php
@@ -10,8 +10,8 @@ set('keep_releases', 4);                                  // Number of releases 
 set('default_timeout', 1200);
 
 add('shared_files', ['.env']);                            // Shared files between deploys
-add('shared_dirs', ['storage','vendor','node_modules']);  // Shared dirs between deploys
-add('writable_dirs', ['storage','vendor','node_modules']);                        // Writable dirs by web server
+add('shared_dirs', ['storage', 'vendor', 'node_modules']);  // Shared dirs between deploys
+add('writable_dirs', ['storage', 'vendor', 'node_modules']);                        // Writable dirs by web server
 
 // **********************************************************************************
 // Task Definitions
@@ -45,10 +45,10 @@ task('reload:php-fpm', function () {
     if ($stage === 'dev') {
         run('sudo /usr/sbin/service php7.4-fpm reload');
     }
-    if ($stage === 'stage'){
+    if ($stage === 'stage') {
         run('sudo /usr/sbin/service php7.4-fpm reload');
     }
-    if ($stage === 'prod'){
+    if ($stage === 'prod') {
         run('sudo /usr/sbin/service php7.4-fpm reload');
     }
 });
@@ -64,11 +64,11 @@ task('reload:supervisor', function () {
 // install npm
 task('npm:install', function () {
     cd('{{release_path}}');
-    run ('./provision/scripts/buildsys.sh');
+    run('./provision/scripts/buildsys.sh');
 })->desc('Running NPM Install');
 
-task( 'npm-build-sys', function () {
-    if ( askConfirmation( 'Are you sure you want to build assets ?' ) ) {
+task('npm-build-sys', function () {
+    if (askConfirmation('Are you sure you want to build assets ?')) {
         invoke('npm:install');
         invoke('npm:build');
     }
@@ -77,25 +77,24 @@ task( 'npm-build-sys', function () {
 // Build using laravel mix
 task('npm:build', function () {
     cd('{{release_path}}');
-    run ('./provision/scripts/refreshbuild.sh');
+    run('./provision/scripts/refreshbuild.sh');
 })->desc('Reloading NPM');
 
-task( 'migrate', function () {
-    if ( askConfirmation( 'Are you sure you want to run migrations?' ) ) {
+task('migrate', function () {
+    if (askConfirmation('Are you sure you want to run migrations?')) {
         invoke('artisan:migrate:fresh');
         invoke('artisan:db:seed');
     }
 })->desc('Migrating Database');
 
 // if deploy to production, then ask to be sure
-task( 'cache-clean', function () {
+task('cache-clean', function () {
 //    if ( askConfirmation( 'Are you sure you want to  clear system cache ?' ) ) {
-        invoke('artisan:cache:clear');
-        invoke('artisan:view:clear');
-        invoke('artisan:config:cache');
+    invoke('artisan:cache:clear');
+    invoke('artisan:view:clear');
+    invoke('artisan:config:cache');
 //    }
 })->desc('Clearing System Cache');
-
 
 // **********************************************************************************
 // Host Definitions
@@ -109,7 +108,7 @@ host('prod')
     ->set('http_user', 'www-data')
     ->set('use_relative_symlink', false)
     ->set('branch', 'master')
-    ->set('composer_options','{{composer_action}} --verbose --no-dev --prefer-dist --no-interaction')
+    ->set('composer_options', '{{composer_action}} --verbose --no-dev --prefer-dist --no-interaction')
     ->set('deploy_path', '/var/www/digiqrp')
     ->set('ssh_multiplexing', true)
     ->set('git_tty', false)                         // [Optional] Allocate tty for git clone. Default value is false.
@@ -124,7 +123,7 @@ host('dev')
     ->set('http_user', 'mag')
     ->set('use_relative_symlink', false)
     ->set('branch', 'develop')
-    ->set('composer_options','{{composer_action}} --verbose --prefer-dist --no-interaction')
+    ->set('composer_options', '{{composer_action}} --verbose --prefer-dist --no-interaction')
     ->set('deploy_path', '/var/www/dev/hamcore')
     ->set('ssh_multiplexing', true)
     ->set('git_tty', false)                         // [Optional] Allocate tty for git clone. Default value is false.
@@ -139,7 +138,7 @@ after('deploy:failed', 'deploy:unlock');
 
 //after('deploy:permissions', 'migrate');
 
-after('deploy:permissions','cache-clean');
+after('deploy:permissions', 'cache-clean');
 
 //after('deploy:permissions','npm-build-sys');
 

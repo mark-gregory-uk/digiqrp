deaf932eb2096dc0e812665917c11093544887ca
Mark Gregory <mark.gregory@mccann.com>
2021-04-18 01:06:25 +0100

    fixing issue with mobile logbook view not displaying correctly

--- Themes/Prostar/views/partials/head.blade.php
+++ Themes/Prostar/views/partials/head.blade.php
@@ -32,8 +32,12 @@
     @stack('css-stack')
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
-    <script src="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.7/css/responsive.dataTables.min.css"></script>
+
+    <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/responsive/2.2.7/css/responsive.dataTables.min.css" rel="stylesheet">
+
 
 
     {!! Theme::script('js/all.js') !!}

aaa2f9416a27b7fc7b825958241811e4fab84af2
Mark Gregory <mark.gregory@mccann.com>
2021-04-18 07:32:44 +0100

    fixing layout issues with mobile logbook view

--- Themes/Prostar/views/logbook.blade.php
+++ Themes/Prostar/views/logbook.blade.php
@@ -39,6 +39,7 @@
 
 
 @section('content')
+    <div class="well">
     <div>
         <h3>Current Digital Mode Log Entries</h3>
         <p>These are live multi-band low power log entries for G4LCH using Xeigu G90 and multi-band vertical. </p>
@@ -60,7 +61,7 @@
             </tbody>
         </table>
     </div>
-
+    </div>
     <script type="text/javascript">
         $(function () {
             var table = $('.data-table').DataTable({

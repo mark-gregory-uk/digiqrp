775fed48c60d0e6933b7c1a3f4040486e2b63742
Mark Gregory <mark.gregory@mccann.com>
2021-04-24 22:54:21 +0100

    Added File Upload Control

--- Modules/Logbook/Http/frontendRoutes.php
+++ Modules/Logbook/Http/frontendRoutes.php
@@ -1,3 +1,7 @@
 <?php
 
 Route::get('logentries', 'LogbookController@all')->name('logbook.all');
+
+Route::get('/upload-file', [\Modules\Logbook\Http\Controllers\Admin\LogbookController::class, 'createForm']);
+
+Route::post('/upload-file', [\Modules\Logbook\Http\Controllers\Admin\LogbookController::class, 'fileUpload'])->name('fileUpload');
---
+++ Modules/Logbook/Resources/views/admin/logbooks/fileupload.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.master')
+
+@section('content-header')
+    <h1>
+        {{ trans('logbook::logbooks.title.logbooks') }}
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="{{ route('dashboard.index') }}"><i class="fa fa-dashboard"></i> {{ trans('core::core.breadcrumb.home') }}</a></li>
+        <li class="active">{{ trans('logbook::logbooks.title.logbooks') }}</li>
+    </ol>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="row">
+                <div style="width: 85%;" class="container">
+                    <form action="{{route('fileUpload')}}" method="post" enctype="multipart/form-data">
+                        <h3 class="text-center mb-5">Upload File in Laravel</h3>
+                        @csrf
+                        @if ($message = Session::get('success'))
+                            <div class="alert alert-success">
+                                <strong>{{ $message }}</strong>
+                            </div>
+                        @endif
+
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <div class="custom-file">
+                            <input type="file" name="file" class="custom-file-input" id="chooseFile">
+
+                            <button  type="submit" name="submit" class="btn btn-primary btn-flat">
+                                Upload Logbook File
+                            </button>
+                        </div>
+
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('footer')
+    <a data-toggle="modal" data-target="#keyboardShortcutsModal"><i class="fa fa-keyboard-o"></i></a> &nbsp;
+@stop
+@section('shortcuts')
+    <dl class="dl-horizontal">
+        <dt><code>c</code></dt>
+        <dd>{{ trans('logbook::logbooks.title.create logbook') }}</dd>
+    </dl>
+@stop
+

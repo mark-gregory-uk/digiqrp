e9aa152d697cb79f1b83cebdcb5e30ce45c0705b
Mark Gregory <mark.gregory@mccann.com>
2021-04-29 09:10:38 +0100

    Fixed Duplicate status messages

--- Modules/Logbook/Resources/views/admin/logbooks/fileupload.blade.php
+++ Modules/Logbook/Resources/views/admin/logbooks/fileupload.blade.php
@@ -18,21 +18,6 @@
                     <form action="{{route('logbook.upload')}}" method="post" enctype="multipart/form-data">
                         <h3 class="text-center mb-5">Upload File in Laravel</h3>
                         @csrf
-                        @if ($message = Session::get('success'))
-                            <div class="alert alert-success">
-                                <strong>{{ $message }}</strong>
-                            </div>
-                        @endif
-
-                        @if (count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
 
                         <div class="custom-file">
                             <input type="file" name="file" class="custom-file-input" id="chooseFile">

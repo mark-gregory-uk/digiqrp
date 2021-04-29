d2c61ad8be9a874b886aa1b1abae77c6cb6ca127
Mark Gregory <mark.gregory@mccann.com>
2021-04-29 14:08:51 +0100

    Removed old Solar Module

--- Modules/Solar/Http/frontendRoutes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Routing\Router;
-/** @var Router $router */
-
-
-Route::get('/solar_data', 'PublicSolarDataController@index')->name('solar.data');
--- Modules/Solar/Repositories/SolarDataRowRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Modules\Solar\Repositories;
-
-use Modules\Core\Repositories\BaseRepository;
-
-interface SolarDataRowRepository extends BaseRepository
-{
-}

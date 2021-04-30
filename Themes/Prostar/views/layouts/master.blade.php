1c40b9c4f4c4765c953a78fe61b49c3fa81773a7
Mark Gregory <mark.gregory@mccann.com>
2021-04-30 10:44:13 +0100

    Added country flags

--- Themes/Prostar/views/layouts/master.blade.php
+++ Themes/Prostar/views/layouts/master.blade.php
@@ -64,7 +64,7 @@
 
                                 {!! '<span>'.$contact->call.'</span><span>&nbsp;</span><span style="float:right;">'.$contact->band_rx.'</span>' !!}</span>
                             @if($contact->country_slug)
-                                <img src="{{ Theme::url('img/flags/png/'.$contact->country_slug.'.png') }}">
+                                <img src="{{ Theme::url('img/flags/png/'.strtolower($contact->country_slug).'.png') }}">
                             @endif
                         </li>
                     @endforeach
@@ -80,7 +80,7 @@
                     @foreach($furthestContacts as $contact)
                         <li itemscope="" itemtype="https://schema.org/Article">
                             @if($contact->country_slug)
-                                <img src="{{ Theme::url('img/flags/png/'.$contact->country_slug.'.png') }}">
+                                <img src="{{ Theme::url('img/flags/png/'.strtolower($contact->country_slug).'.png') }}">
                             @endif
                             <span itemprop="name">{!! '&nbsp;&nbsp;<span>'.$contact->call.'</span><span>&nbsp;</span><span style="float:right;">'.round($contact->distance_km).' Km</span>' !!}</>
                         </li>

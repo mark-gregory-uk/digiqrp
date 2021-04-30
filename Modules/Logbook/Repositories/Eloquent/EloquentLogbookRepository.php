aa4ecc0de10e0a85892d285e5cba625c7c523b86
Mark Gregory <mark.gregory@mccann.com>
2021-04-30 09:46:15 +0100

    Added distance fields and display

--- Modules/Logbook/Repositories/Eloquent/EloquentLogbookRepository.php
+++ Modules/Logbook/Repositories/Eloquent/EloquentLogbookRepository.php
@@ -16,12 +16,14 @@ class EloquentLogbookRepository extends EloquentBaseRepository implements Logboo
 
     public function latestContacts()
     {
-        $defaultLogBook = $this->where('owner_id', Auth::id())->where('default', true)->first();
+        $user = 1;
+        $defaultLogBook = $this->where('owner_id', $user)->where('default', true)->first();
         return  $defaultLogBook->entries()->orderBy('qso_start', 'desc')->take(5)->get();
     }
 
     public function longestContacts(){
-        $defaultLogBook = $this->where('owner_id', Auth::id())->where('default', true)->first();
+        $user = 1;
+        $defaultLogBook = $this->where('owner_id', $user)->where('default', true)->first();
         return  $defaultLogBook->entries()->orderBy('distance_km', 'desc')->take(5)->get();
     }
 

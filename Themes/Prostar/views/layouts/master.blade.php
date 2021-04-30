63e8dd78e5b7a8794fbf5290f306cba3c90219c2
Mark Gregory <mark.gregory@mccann.com>
2021-04-30 10:51:40 +0100

    minor layout fix

--- Themes/Prostar/views/layouts/master.blade.php
+++ Themes/Prostar/views/layouts/master.blade.php
@@ -82,7 +82,7 @@
                             @if($contact->country_slug)
                                 <img src="{{ Theme::url('img/flags/png/'.strtolower($contact->country_slug).'.png') }}">
                             @endif
-                            <span itemprop="name">{!! '&nbsp;&nbsp;<span>'.$contact->call.'</span><span>&nbsp;</span><span style="float:right;">'.round($contact->distance_km).' Km</span>' !!}</>
+                            <span itemprop="name">{!! '&nbsp;&nbsp;<span>'.$contact->call.'</span><span>&nbsp;</span><span style="float:right;">'.round($contact->distance_km).' Km</span>' !!}
                         </li>
                     @endforeach
 

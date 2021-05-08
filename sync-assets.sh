5a73f302b9147074147518b662e62bf1280e759f
Mark Gregory <mark.gregory@mccann.com>
2021-05-08 09:42:33 +0100

    Added command to sync assets

---
+++ sync-assets.sh
@@ -0,0 +1,2 @@
+rsync -azhv root@194.146.44.118://var/www/digiqrp/current/public/storage/media/* ./storage/app/public/media
+rsync -azhv root@194.146.44.118://var/www/digiqrp/current/public/storage/uploads/* ./storage/app/public/uploads

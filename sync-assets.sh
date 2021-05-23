9e75063681130f70cc0f8f3d38aab3f44f43cd70
Mark Gregory <mag@Marks-MacBook-Pro.local>
2021-05-23 14:43:35 +0100

    Added Docker Support

--- sync-assets.sh
+++ sync-assets.sh
@@ -1,4 +1,4 @@
 rsync -azhv root@194.146.44.118://var/www/digiqrp/current/public/storage/media/* ./storage/app/public/media
 rsync -azhv root@194.146.44.118://var/www/digiqrp/current/public/storage/uploads/* ./storage/app/public/uploads
 rsync -azhv root@194.146.44.118://var/www/digiqrp/current/public/storage/uploads/* ./storage/app/public/uploads
-rsync -azhv root@194.146.44.118://var/www/digiqrp/current/Laravel/* ./storage/backups
+# rsync -azhv root@194.146.44.118://var/www/digiqrp/current/Laravel/* ./storage/backups

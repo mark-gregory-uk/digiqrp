3e7773da32073ae18c191b607d4c758316edc30e
Mark Gregory <mark.gregory@gmx.com>
2021-08-02 21:10:32 +0100

    Fixed lack of side menus

--- Modules/Page/Http/Controllers/PublicController.php
+++ Modules/Page/Http/Controllers/PublicController.php
@@ -3,10 +3,16 @@
 namespace Modules\Page\Http\Controllers;
 
 use Illuminate\Contracts\Foundation\Application;
+use Modules\Blog\Entities\Post;
+use Modules\Blog\Repositories\PostRepository;
 use Modules\Core\Http\Controllers\BasePublicController;
+use Modules\Logbook\Entities\Logbook;
+use Modules\Logbook\Repositories\LogbookRepository;
+use Modules\Menu\Entities\Menu;
 use Modules\Menu\Repositories\MenuItemRepository;
 use Modules\Page\Entities\Page;
 use Modules\Page\Repositories\PageRepository;
+use Modules\Solar\Repositories\SolarRepository;
 
 class PublicController extends BasePublicController
 {
@@ -14,6 +20,22 @@ class PublicController extends BasePublicController
      * @var PageRepository
      */
     private $page;
+
+    /**
+     * @var Post
+     */
+    private $postRepository;
+
+    /**
+     * @var Logbook
+     */
+    private $logbookRepository;
+
+    /**
+     * @var SolarRepository
+     */
+    private $solarReportsRepository;
+
     /**
      * @var Application
      */
@@ -21,21 +43,28 @@ class PublicController extends BasePublicController
 
     private $disabledPage = false;
 
-    public function __construct(PageRepository $page, Application $app)
+    public function __construct(PageRepository $page, PostRepository $postsRepository, LogbookRepository $logBookRepository, SolarRepository $solarRepository, Application $app)
     {
         parent::__construct();
         $this->page = $page;
         $this->app = $app;
+        $this->postRepository = $postsRepository;
+        $this->logbookRepository = $logBookRepository;
+        $this->solarReportsRepository = $solarRepository;
     }
 
     /**
      * @param $slug
+     *
      * @return \Illuminate\View\View
      */
     public function uri($slug)
     {
         $page = $this->findPageForSlug($slug);
-
+        $latestPosts = $this->postRepository->latest();
+        $latestContacts = $this->logbookRepository->latestContacts();
+        $furthestContacts = $this->logbookRepository->longestContacts();
+        $latestSolarReports = $this->solarReportsRepository->latestReports();
         $this->throw404IfNotFound($page);
 
         $currentTranslatedPage = $page->getTranslation(locale());
@@ -47,7 +76,7 @@ class PublicController extends BasePublicController
 
         $this->addAlternateUrls($this->getAlternateMetaData($page));
 
-        return view($template, compact('page'));
+        return view($template, compact('page', 'latestPosts', 'latestContacts', 'latestSolarReports', 'furthestContacts'));
     }
 
     /**
@@ -56,20 +85,25 @@ class PublicController extends BasePublicController
     public function homepage()
     {
         $page = $this->page->findHomepage();
-
+        $latestPosts = $this->postRepository->latest();
+        $latestContacts = $this->logbookRepository->latestContacts();
+        $furthestContacts = $this->logbookRepository->longestContacts();
+        $latestSolarReports = $this->solarReportsRepository->latestReports();
         $this->throw404IfNotFound($page);
 
         $template = $this->getTemplateForPage($page);
 
         $this->addAlternateUrls($this->getAlternateMetaData($page));
 
-        return view($template, compact('page'));
+        return view($template, compact('page', 'latestPosts', 'latestContacts', 'latestSolarReports', 'furthestContacts'));
     }
 
     /**
      * Find a page for the given slug.
      * The slug can be a 'composed' slug via the Menu.
+     *
      * @param string $slug
+     *
      * @return Page
      */
     private function findPageForSlug($slug)
@@ -86,7 +120,9 @@ class PublicController extends BasePublicController
     /**
      * Return the template for the given page
      * or the default template if none found.
+     *
      * @param $page
+     *
      * @return string
      */
     private function getTemplateForPage($page)
@@ -96,6 +132,7 @@ class PublicController extends BasePublicController
 
     /**
      * Throw a 404 error page if the given page is not found or draft.
+     *
      * @param $page
      */
     private function throw404IfNotFound($page)
@@ -122,5 +159,51 @@ class PublicController extends BasePublicController
         }
 
         return $alternate;
+    }
+
+    /**
+     * Generates the systems site map.
+     *
+     * @return mixed
+     */
+    public function sitemap()
+    {
+        // create new sitemap object
+        $sitemap = App::make('sitemap');
+
+        // set cache key (string), duration in minutes (Carbon|Datetime|int), turn on/off (boolean)
+        // by default cache is disabled
+        $sitemap->setCache('laravel.sitemap', 60);
+
+        // check if there is cached sitemap and build new only if is not
+        if (! $sitemap->isCached()) {
+            // add item to the sitemap (url, date, priority, freq)
+            $sitemap->add(URL::to('/'), '2012-08-25T20:10:00+02:00', '1.0', 'daily');
+            $sitemap->add(URL::to('/logbook/index'), '2012-08-26T12:30:00+02:00', '0.9', 'weekly');
+
+            // get all posts from db, with image relations
+            $pages = \DB::table('pages')->orderBy('created_at', 'desc')->get();
+
+            // add every page to the sitemap
+            foreach ($pages as $page) {
+                $sitemap->add(URL::to('page /'.$page->slug), '2012-08-26T12:30:00+02:00', '0.9', 'weekly');
+            }
+
+            // add every post to the sitemap
+            $posts = \DB::table('posts')->orderBy('created_at', 'desc')->get();
+
+            foreach ($posts as $post) {
+                $sitemap->add(URL::to('post/'.$post->id), '2012-08-26T12:30:00+02:00', '0.9', 'daily');
+            }
+
+            $newsItems = \DB::table('news_items')->orderBy('created_at', 'desc')->get();
+
+            foreach ($newsItems as $news) {
+                $sitemap->add(URL::to('news/'.$news->id), '2012-08-26T12:30:00+02:00', '0.9', 'daily');
+            }
+        }
+
+        // show your sitemap (options: 'xml' (default), 'html', 'txt', 'ror-rss', 'ror-rdf')
+        return $sitemap->render('xml');
     }
 }

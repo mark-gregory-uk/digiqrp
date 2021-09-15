8ffd563a632df6307d74b60f4a37d22ade734596
Mark Gregory <mark.gregory@mccann.com>
2021-09-16 00:22:12 +0100

    widget mods

--- Themes/Prostar/views/layouts/master.blade.php
+++ Themes/Prostar/views/layouts/master.blade.php
@@ -11,6 +11,7 @@
                     <span style="margin-right: -4px;font-size: 11px;font-weight: bold;background-color:  #555555; color:whitesmoke;border-top-left-radius: 3px;border-bottom-left-radius: 4px;padding-left: 3px;padding-right: 3px;padding-top: 3px;padding-bottom: 3px;">Version</span>
                     <span style="font-size: 11px;font-weight: bold;background-color:#6ec83f; color:whitesmoke;border-top-right-radius: 4px;border-bottom-right-radius: 4px;padding: 3px;" >{{Version::nocommit()}}</span>
                     @include('cookieConsent::index')
+                    {{ Request::path() }}
                 </p>
                 @include('partials.navigation')
             <!-- Begin Breadcrumbs -->
@@ -24,9 +25,6 @@
                     <div id="system-message-container">
                        @include('partials.system-messages')
                     </div>
-
-
-
                     <div class="blog" itemscope itemtype="https://schema.org/Blog">
                         @yield('content')
                     </div>
@@ -34,14 +32,17 @@
             </main>
             <div id="aside" class="span3">
                 <!-- Begin Right Sidebar -->
-                @include('partials.latest-posts')
-                @include('partials.latest-contacts')
                 @include('partials.calllookup')
+                @include('partials.latest-posts')
+                @if (Request::path() != 'welcome')
+                    @include('partials.latest-contacts')
+                @endif
                 @include('partials.furthest-contacts')
-                @include('partials.logchart')
-                @include('partials.solar-report')
-
-                @if (Request::path() != 'logbook-stats')
+                @if (Request::path() != 'welcome')
+                  @include('partials.logchart')
+                  @include('partials.solar-report')
+                @endif
+                @if ( (Request::path() != 'welcome') and (Request::path() != 'logbook-stats'))
                     @include('partials.sunspot-trend')
                 @endif
 

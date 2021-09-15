37ad3d92ce13836e1cf91662e633e14000198330
Mark Gregory <mark.gregory@mccann.com>
2021-09-16 00:23:07 +0100

    widget mods

--- Themes/Prostar/views/layouts/master.blade.php
+++ Themes/Prostar/views/layouts/master.blade.php
@@ -11,7 +11,6 @@
                     <span style="margin-right: -4px;font-size: 11px;font-weight: bold;background-color:  #555555; color:whitesmoke;border-top-left-radius: 3px;border-bottom-left-radius: 4px;padding-left: 3px;padding-right: 3px;padding-top: 3px;padding-bottom: 3px;">Version</span>
                     <span style="font-size: 11px;font-weight: bold;background-color:#6ec83f; color:whitesmoke;border-top-right-radius: 4px;border-bottom-right-radius: 4px;padding: 3px;" >{{Version::nocommit()}}</span>
                     @include('cookieConsent::index')
-                    {{ Request::path() }}
                 </p>
                 @include('partials.navigation')
             <!-- Begin Breadcrumbs -->

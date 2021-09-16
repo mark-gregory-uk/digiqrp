1a4a50b311392ecb00120cc61c6b871a4082c59e
Mark Gregory <mark.gregory@mccann.com>
2021-09-16 17:07:01 +0100

    updated base class

--- Modules/Notification/Services/Notification.php
+++ Modules/Notification/Services/Notification.php
@@ -14,6 +14,24 @@ interface Notification
     public function push($title, $message, $icon, $link = null);
 
     /**
+     * Push a notification on the dashboard to a specific user
+     * @param string $title
+     * @param string $message
+     * @param string $icon
+     * @param string|null $link
+     */
+    public function pushToUser($title, $message, $icon, $link = null,$user);
+
+    /**
+     * Push a notification on the dashboard to all admins
+     * @param string $title
+     * @param string $message
+     * @param string $icon
+     * @param string|null $link
+     */
+    public function pushToAdmins($title, $message, $icon, $link = null);
+
+    /**
      * Set a user id to set the notification to a specific user
      * @param int $userId
      * @return $this

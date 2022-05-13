b93601bed73b505911b0ba0aa87886afb695b77d
Mark Gregory <mark.gregory@mccann.com>
2022-05-13 09:31:40 +0100

    added error files

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <!--
+    <section class="page-error-image page-error-image--404">
+        <img src="/img/bentley404.jpg">
+    </section>
+    -->
+    <section class="page-error">
+        <div class="content">
+            <div class="col-1-1">
+                <h1 class="header header--one header--strike">404</h1>
+                <p>Sorry the page you're looking for cannot be found</p>
+            </div>
+        </div>
+    </section>
+
+@stop
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <!--
+    <section class="page-error-image page-error-image--404">
+        <img src="/img/bentley404.jpg">
+    </section>
+    -->
+    <section class="page-error">
+        <div class="content">
+            <div class="col-1-1">
+                <h1 class="header header--one header--strike">500</h1>
+                <p>Sorry there has been an issue with the request</p>
+            </div>
+        </div>
+    </section>
+
+@stop
